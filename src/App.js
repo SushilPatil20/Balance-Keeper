9276b2b62c1245a4a1fb750ac2e0b4dce586fc78
PatilSushil20 <124390002+PatilSushil20@users.noreply.github.com>
2024-07-26 08:54:01 +0530

    Converted Unordered list into table (Small changes is the UI)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 
 function App() {
   const [cards, setCard] = useState(() => {
-    const saveCards = localStorage.getItem("cards");
-    return saveCards ? JSON.parse(saveCards) : [];
+    return JSON.parse(localStorage.getItem("cards")) || [];
+    // return saveCards ?  : [];
   });
   const [title, setTitle] = useState("");
   const [amount, seAmount] = useState("");
 
-  // Store it into the localstorage
+  // whenever cards is are updated we Store the cards
   useEffect(() => {
     localStorage.setItem("cards", JSON.stringify(cards));
   }, [cards]);
@@ -29,9 +29,9 @@ function App() {
     seAmount("");
   };
 
-  function formateCurrency(currency) {
-    return `${currency} ₹`;
-  }
+  const formateCurrency = (currency) => {
+    return `${currency.toLocaleString("en-IN")} ₹`;
+  };
 
   function removeCard(index) {
     const newArray = cards.filter((_, idx) => idx !== index);
@@ -43,58 +43,86 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header flex items-center flex-col">
-        <h2 className="my-14 text-white text-2xl">
-          Total Unpaid : {formateCurrency(totalAmount)}
-        </h2>
-        <form className="min-h-40 space-y-8  w-1/2" onSubmit={onSubmit}>
-          <section className="space-y-2">
-            <label htmlFor="title" className="block text-3xl text-white">
-              Title
-            </label>
-            <input
-              type="text"
-              className="p-2 w-full outline-none"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              id="title"
-            />
-          </section>
-          <section className="space-y-2">
-            <label htmlFor="mony" className="block text-3xl text-white">
-              Amount
-            </label>
-            <input
-              type="number"
-              className="p-2 w-full outline-none"
-              value={amount}
-              onChange={(e) => seAmount(e.target.value)}
-              id="mony"
-            />
-          </section>
-          <button
-            type="submit"
-            className="px-8 py-2 text-xl bg-white 
+      <header className="App-header px-8">
+        <section className="flex flex-col md:flex-row  md:space-x-14">
+          <section className="w-full  flex items-center flex-col md:w-1/2 md:h-screen ">
+            <h2 className="my-7 text-white text-2xl">--- Balance-Keeper ---</h2>
+            <form className="min-h-40 space-y-8 w-full" onSubmit={onSubmit}>
+              <section className="space-y-2">
+                <label htmlFor="title" className="block text-3xl text-white">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  className="p-2 w-full outline-none"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  id="title"
+                />
+              </section>
+              <section className="space-y-2">
+                <label htmlFor="mony" className="block text-3xl text-white">
+                  Amount
+                </label>
+                <input
+                  type="number"
+                  className="p-2 w-full outline-none"
+                  value={amount}
+                  onChange={(e) => seAmount(e.target.value)}
+                  id="mony"
+                />
+              </section>
+              <button
+                type="submit"
+                className="px-8 py-2 text-xl bg-white 
           active:scale-105 rounded-md text-gray-800 mx-auto block"
-          >
-            Submit
-          </button>
-        </form>
-        <section className="mt-12 w-1/2 space-y-3">
-          <h2 className="text-white text-2xl">Taken from</h2>
-          <section className="flex">
-            <ul className="text-white w-full space-y-4">
-              {cards.map((card, index) => (
-                <li
-                  key={index}
-                  className="bg-black h-12 w-full flex items-center justify-between px-4"
-                  onDoubleClick={() => removeCard(index)}
-                >
-                  <p>{card.title}</p>
-                  <strong>{formateCurrency(card.amount)}</strong>
-                </li>
-              ))}
-            </ul>
+              >
+                Submit
+              </button>
+            </form>
+          </section>
+
+          <section className="w-full md:w-1/2 overflow-y-auto min-h-screen">
+            <section className="mt-4 md:mt-8 space-y-3">
+              <div className="flex justify-between items-center">
+                <h2 className="text-white text-2xl">Taken from</h2>
+                <h2 className="text-center text-white">
+                  Total Unpaid Amount : {formateCurrency(totalAmount)}
+                </h2>
+              </div>
+              <div className="overflow-y-auto max-h-60 relative">
+                <table className="min-w-full bg-white border border-gray-200">
+                  <thead className="sticky top-0">
+                    <tr>
+                      <th className="py-2 px-4 border-b border-gray-200 bg-gray-50">
+                        Index
+                      </th>
+                      <th className="py-2 px-4 border-b border-gray-200 bg-gray-50">
+                        Title
+                      </th>
+                      <th className="py-2 px-4 border-b border-gray-200 bg-gray-50">
+                        Amount
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cards.map((card, index) => (
+                      <tr key={index} onDoubleClick={() => removeCard(index)}>
+                        <td className="py-2 px-4 border-b border-gray-200 text-center">
+                          {index + 1}
+                        </td>
+                        <td className="py-2 px-4 border-b border-gray-200 text-center">
+                          {card.title}
+                        </td>
+                        <td className="py-2 px-4 border-b border-gray-200 text-center">
+                          {formateCurrency(card.amount)}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </section>
           </section>
         </section>
       </header>
